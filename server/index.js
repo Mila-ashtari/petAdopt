65a06ea6515eacf598f54471537066ab0bad49f1
Mila Ashtari <mashtari1991@gmail.com>
2021-04-20 11:11:44 -0400

    added mongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose=require('mongoose')
+const keys = require('./config/keys');
 require('./services/passport');
 
 const app = express();
+mongoose.connect(keys.mongoURI)
+
 
 require('./routes/authRoutes')(app);
 
