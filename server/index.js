7480fc299f530597ac0bdab62d9736c18696da4c
Mila Ashtari <mashtari1991@gmail.com>
2021-05-11 15:01:50 -0400

    created users collection on mongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
-const app = express();
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
 
+const app = express();
 
 require('./routes/authRoutes')(app);
 
